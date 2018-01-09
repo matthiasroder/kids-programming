f36b6d84dc29f65ec67d47814b7e6dc005522c34
Matthias Röder <mr1977@gmail.com>
2018-01-09 20:44:04 +0100

    day1

---
+++ day01/counting.py
@@ -0,0 +1,23 @@
+x = 1
+
+while x < 5:
+	print "Pferd"
+	x = x + 1
+
+while x < 5:
+	print x
+	x = x + 1
+
+def clara(x):
+	while x < 5:
+		print x
+		x = x + 1
+
+clara(1)
+
+def clara(x, z):
+	while x < z:
+		print x
+		x = x + 1
+
+clara(1,100)
